bf651afb787c78fa23367bea47e7396d0a4ad109
sathishkumar <sathishkumarramalingam14@gmail.com>
2023-06-19 16:23:46 +0530

    Adding solutions

---
+++ Module1/Day4/solution7.c
@@ -0,0 +1,23 @@
+// Solution 2 - String convert string as integer
+#include <stdio.h>
+#include<string.h>
+
+int main()
+{
+    char str[] = "12345";  // Predefined string of digits
+    int result = 0;
+    int i = 0;
+
+    while (str[i] != '\0')
+    {
+        int ival = str[i] - '0';
+        result = result * 10 + ival;
+        i++;
+    }
+
+    printf("String: %s\n", str);
+    printf("Integer: %d\n", result);
+
+    return 0;
+}
+
