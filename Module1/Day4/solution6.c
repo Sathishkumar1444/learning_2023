88499902bee6697a29cd05e4dd2528110d5c2100
sathishkumar <sathishkumarramalingam14@gmail.com>
2023-06-19 16:22:27 +0530

    Adding solutions

---
+++ Module1/Day4/solution6.c
@@ -0,0 +1,27 @@
+// Solution 1 - String toggle case
+#include <stdio.h>
+#include <ctype.h>
+#include<string.h>
+int main()
+{
+    char str[] = "Hello, World!";  // Predefined string
+    int i = 0;
+
+    while (str[i])
+    {
+        if (isupper(str[i]))
+        {
+            str[i] = tolower(str[i]);
+        }
+        else if (islower(str[i]))
+        {
+            str[i] = toupper(str[i]);
+        }
+        i++;
+    }
+
+    printf("Toggled case: %s\n", str);
+
+    return 0;
+}
+
